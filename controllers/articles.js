0ed7f8e328547adabd53e4fc298f2cfbede88f0c
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-11-18 09:38:10 +0300

    linter problems fixed

--- controllers/articles.js
+++ controllers/articles.js
@@ -6,26 +6,27 @@ module.exports.getArticles = (req, res, next) => {
   const ownerId = req.user._id;
   Article.AddOwnerId()
     .then((articles) => {
-
-      const meArticles = articles.filter((article, i) => {
-        return article.owner.equals(ownerId)
-      })
+      const meArticles = articles.filter((article) => article.owner.equals(ownerId));
 
       const meArticlesNoOwner = meArticles.map((article) => {
-        article = article.toObject();
-        delete article.owner;
-        return article;
+        const articleObj = article.toObject();
+        delete articleObj.owner;
+        return articleObj;
       });
 
-      res.send({ data: meArticlesNoOwner })
+      res.send({ data: meArticlesNoOwner });
     })
     .catch(next);
 };
 
 module.exports.createArticle = (req, res, next) => {
-  const { keyword, title, text, date, source, link, image } = req.body;
+  const {
+    keyword, title, text, date, source, link, image,
+  } = req.body;
   const owner = req.user._id;
-  Article.create({ keyword, title, text, date, source, link, image, owner })
+  Article.create({
+    keyword, title, text, date, source, link, image, owner,
+  })
     .then((article) => res.send({ data: article }))
     .catch(next);
 };
@@ -37,11 +38,11 @@ module.exports.delArticleById = (req, res, next) => {
       if (!article) throw new NotFoundError('Статья не найдена');
       if (!article.owner.equals(req.user._id)) throw new ForbiddenError('Вы не можете удалять чужую статью');
       Article.findByIdAndRemove(articleId)
-        .then((article) => {
-          article = article.toObject();
-          delete article.owner;
-          return res.send({ data: article });
-        })
+        .then((articleToRemove) => {
+          const articleToRemoveObj = articleToRemove.toObject();
+          delete articleToRemoveObj.owner;
+          return res.send({ data: articleToRemoveObj });
+        });
     })
     .catch(next);
 };
